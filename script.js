5527fa469706ba253ec21df6a1c330342352a02b
vladbranduse <vladbranduse@gmail.com>
2024-12-13 14:27:06 +0200

    Setup the project structure

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+  
+function getComputerChoice() {
+    getRandomInt(3);
+    if (getRandomInt(3) === 0) {
+        return "Rock";
+    } else if (getRandomInt(3) === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
